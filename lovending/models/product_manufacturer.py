b3537f90ed887d2d18df1c4338599f9ef9bfdfc2
Adriana <aortiz@marstom.com>
2022-05-25 10:03:17 -0500

    Se agrega modulo lovending

---
+++ lovending/models/product_manufacturer.py
@@ -0,0 +1,17 @@
+# Copyright 2009 NetAndCo (<http://www.netandco.net>).
+# Copyright 2011 Akretion Benoît Guillot <benoit.guillot@akretion.com>
+# Copyright 2014 prisnet.ch Seraphine Lantible <s.lantible@gmail.com>
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2018 Daniel Campos <danielcampos@avanzosc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import api, fields, models
+
+
+class ProductManufacturer(models.Model):
+    _name = "product.manufacturer"
+    _description = "Product manufacturer"
+    _order = "name"
+
+    name = fields.Char("Nombre del fabricante", required=True)
+    description = fields.Text(translate=True)
